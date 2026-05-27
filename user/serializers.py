00e135f3176f4afc9b3eaed5110bc97f70eaa677
Seroga <ilchuk.cergey@gmail.com>
2026-05-27 14:16:29 +0300

    Implement user manager endpoint

--- user/serializers.py
+++ user/serializers.py
@@ -21,3 +21,36 @@ class UserCreateSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         return get_user_model().objects.create_user(**validated_data)
+
+class UserManagerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = get_user_model()
+        fields = (
+            "id",
+            "email",
+            "password",
+            "first_name",
+            "last_name",
+            "profile_pic",
+            "phone",
+            "birth_date",
+            "bio"
+        )
+        read_only_fields = ("id",)
+        extra_kwargs = {
+            "password": {
+                "write_only": True,
+                "style": {"input_type": "password"},
+                "min_length": 8,
+            }
+        }
+
+        def update(self, instance, validated_data):
+            password = validated_data.pop("password")
+            user = super().update(instance, validated_data)
+
+            if password:
+                user.set_password(password)
+                user.save()
+
+            return user
--- user/urls.py
+++ user/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from user.views import CreateUserView
-
+from user.views import CreateUserView, CreateTokenView, UserManageView
 
 app_name = "user"
 
 urlpatterns = [
     path("register/", CreateUserView.as_view(), name="register"),
+    path("login/", CreateTokenView.as_view(), name="login"),
+    path("me/", UserManageView.as_view(), name="me"),
 ]
