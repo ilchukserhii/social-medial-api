f29564d39470bc784e89ae73cfce79fd047dce69
Seroga <ilchuk.cergey@gmail.com>
2026-05-27 19:30:02 +0300

    Optimized user public view with following/followers count and for details with first 10 following/followers

--- user/serializers.py
+++ user/serializers.py
@@ -66,10 +66,14 @@ class UserManagerSerializer(serializers.ModelSerializer):
         return user
 
 
-class UserPublicSerializer(serializers.ModelSerializer):
-    following = serializers.StringRelatedField(
+class UserPublicListSerializer(serializers.ModelSerializer):
+    following = serializers.IntegerField(
         read_only=True,
-        many=True,
+        source="following.count"
+    )
+    followers = serializers.IntegerField(
+        read_only=True,
+        source="followers.count"
     )
     class Meta:
         model = get_user_model()
@@ -81,4 +85,39 @@ class UserPublicSerializer(serializers.ModelSerializer):
             "profile_pic",
             "bio",
             "following",
+            "followers",
         )
+
+class UserPublicDetailSerializer(UserPublicListSerializer):
+    following_list = serializers.SerializerMethodField()
+    followers_list = serializers.SerializerMethodField()
+    class Meta:
+        model = get_user_model()
+        fields = (
+            "id",
+            "nickname",
+            "first_name",
+            "last_name",
+            "profile_pic",
+            "bio",
+            "following",
+            "following_list",
+            "followers",
+            "followers_list",
+        )
+
+    def get_following_list(self, obj):
+        following_list = obj.following.all()[:10]
+
+        return [
+            following.nickname or str(following)
+            for following in following_list
+        ]
+
+    def get_followers_list(self, obj):
+        followers_list = obj.followers.all()[:10]
+
+        return [
+            follower.nickname or str(follower)
+            for follower in followers_list
+        ]
--- user/views.py
+++ user/views.py
@@ -1,14 +1,25 @@
+from django.contrib.admin import actions
 from django.contrib.auth import get_user_model
+from django.db.models import Q
+from django.template.defaultfilters import first
+from django.views.generic import detail
 from rest_framework import generics, mixins
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.settings import api_settings
 from rest_framework.views import APIView
 from rest_framework.viewsets import GenericViewSet
 
-from user.serializers import UserCreateSerializer, UserManagerSerializer, UserPublicSerializer
+from user.models import User
+from user.serializers import (
+    UserCreateSerializer,
+    UserManagerSerializer,
+    UserPublicListSerializer,
+    UserPublicDetailSerializer
+)
 
 User = get_user_model()
 
@@ -30,6 +41,7 @@ class UserManageView(generics.RetrieveUpdateAPIView):
 
 
 class LogoutView(APIView):
+    authentication_classes = (TokenAuthentication,)
     permission_classes = (IsAuthenticated,)
 
     def post(self, request):
@@ -43,14 +55,46 @@ class UserPublicView(
     GenericViewSet,
 ):
     queryset = User.objects.all()
-    serializer_class = UserPublicSerializer
+    authentication_classes = (TokenAuthentication,)
     permission_classes = (IsAuthenticated,)
 
     def get_queryset(self):
         nickname = self.request.query_params.get("nickname")
+        name = self.request.query_params.get("name")
         queryset = User.objects.all()
 
         if nickname:
             queryset = self.queryset.filter(nickname__icontains=nickname)
 
-        return queryset
+        if name:
+            queryset = self.queryset.filter(
+                Q(first_name__icontains=name) |
+                Q(last_name__icontains=name)
+            )
+
+        return queryset.order_by("-date_joined")
+
+    def get_serializer_class(self):
+        if self.action == "retrieve":
+            return UserPublicDetailSerializer
+        return UserPublicListSerializer
+
+    @action(detail=True, methods=["post"])
+    def follow(self, request, pk=None):
+        user_to_follow = self.get_object()
+        if user_to_follow == request.user:
+            return Response({"message": "You cannot follow yourself"}, status=400)
+        if request.user.following.filter(pk=user_to_follow.id).exists():
+            return Response({"message": "You`re already following this user"})
+        request.user.following.add(user_to_follow)
+        return Response({"message": "You`re now following this user"})
+
+    @action(detail=True, methods=["post"])
+    def unfollow(self, request, pk=None):
+        user_to_unfollow = self.get_object()
+        if request.user.following.filter(pk=user_to_unfollow.id).exists():
+            request.user.following.remove(user_to_unfollow)
+            return Response({"message": "You`re not following this user anymore"})
+        return Response({"message": "You`re not following this user"})
+
+
