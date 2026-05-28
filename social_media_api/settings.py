eb445a916978f9c90fea7ba8a637d8456b54cc52
Seroga <ilchuk.cergey@gmail.com>
2026-05-28 20:00:11 +0300

    Implement default pagination

--- social_media_api/settings.py
+++ social_media_api/settings.py
@@ -132,7 +132,11 @@ MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
 
 REST_FRAMEWORK = {
+    "DEFAULT_PAGINATION_CLASS":
+            "rest_framework.pagination.PageNumberPagination",
+        "PAGE_SIZE": 10,
+
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework.authentication.TokenAuthentication",
-    ]
+    ],
 }
